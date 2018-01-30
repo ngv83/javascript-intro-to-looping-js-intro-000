38c50fadca308bfa3a7e4ff4bd8e613647f4f039
Napoleon Guardado <napo.gv@hotmail.com>
2018-01-30 05:05:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,3 +16,14 @@ function whileLoop(n) {
 
   return 'done'
 }
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
+
+  return array
+}
